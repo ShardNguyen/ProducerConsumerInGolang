7bfe58d85e69e04938fa0c47a3be167e6c365148
ShardNguyen <shardnguyen@gmail.com>
2025-03-07 11:56:08 +0700

    Implemented moderator role

--- main.go
+++ main.go
@@ -1,57 +1,100 @@
+// Task: Change bool condition on loop to channel-based condition kind of loop
+/*
+	- Current solution: Make a moderator role
+
+	- Moderator will be the only channel to trigger the toStop channel
+	- toStop channel will notify the moderator to close Stop channel, meaning the Stop channel will be triggered in the "select" case
+		+ Sending data to a closed channel will cause a panic
+		+ But receiving data from a closed channel will be ok (Meaning <- closedChannel will still trigger the "select")
+	- The Stop channel is used for breaking the loop in both consumer and producer
+*/
+
+// Global variable
+
 package main
 
 import (
 	"fmt"
 	"sync"
-	"time"
+	//"time"
 )
 
 func multiConsumerProducer(producerSize, consumerSize int) {
-	ch := make(chan string) // A channel for communication between producer and consumer
-	var wg sync.WaitGroup   // Wait Group to indicate how many concurrent functions that needs waiting to be done
+	ch := make(chan string)        // A channel for communication between producer and consumer
+	stopCh := make(chan struct{})  // An additional channel to stop data
+	toStopCn := make(chan bool)    // Another channel to notify the moderator to close the stopCh channel
+	var wgConsumers sync.WaitGroup // Wait Group to indicate how many concurrent RECEIVING functions that needs waiting to be done
+
+	// Start one moderator
+	go moderator(stopCh, toStopCn)
 
 	// Start multiple producers
 	for i := 0; i < producerSize; i++ {
-		wg.Add(1) // Indicate that Wait Group needs to wait for one more function
-		go producer(i, ch, &wg)
+		go producer(i, producerSize, ch, stopCh, toStopCn)
 	}
 
 	// Start multiple consumers
 	for i := 0; i < consumerSize; i++ {
-		wg.Add(1) // Indicate that Wait Group needs to wait for one more function
-		go consumer(i, ch, &wg)
+		wgConsumers.Add(1) // Indicate that Wait Group needs to wait for one more function
+		go consumer(i, ch, stopCh, toStopCn, &wgConsumers)
 	}
 
-	wg.Wait() // Wait until every go functions finish their job
+	wgConsumers.Wait() // Wait until every consumers functions finish their job
 	close(ch)
 }
 
-func producer(index int, ch chan string, wg *sync.WaitGroup) {
-	// Each producer will send out 5 messages (As an example)
-	for i := 0; i < 5; i++ {
-		// Sending signal away and block the current iteration (But the next iteration can start somehow?)
-		ch <- fmt.Sprintf("Producer %v send %v", index, i)
-	}
-	// Once all of the producer's messages are received, the job is done for the producer
-	wg.Done()
+func moderator(stopCh chan struct{}, toStopCn chan bool) {
+	<-toStopCn
+	close(stopCh)
 }
 
-func consumer(index int, ch chan string, wg *sync.WaitGroup) {
-	done := false
-	for !done {
-		// The loop is blocked until one of the two scenarios happens
+func producer(index int, prodSize int, ch chan string, stopCh chan struct{}, toStopCn chan bool) {
+	// Each producer will send out 5 messages (As an example)
+	for i := 0; i < 5; i++ {
+		// Logical condition when producer doesn't want to produce any more data
+		if index+1 == prodSize && i == 4 {
+			select {
+			// Send a signal to the moderator for ending the data tranmission
+			case toStopCn <- true:
+				fmt.Println("Stopped by producer #", index, "at msg", i)
+			default:
+			}
+		}
+
+		select {
+		// Stop sending messages when receiving the stop signal
+		case <-stopCh:
+			return
+		// Sending signal away and block the thread of current iteration
+		case ch <- fmt.Sprintf("Producer %v send %v", index, i):
+		}
+	}
+}
+
+func consumer(index int, ch chan string, stopCh chan struct{}, toStopCn chan bool, wg *sync.WaitGroup) {
+consumerLoop:
+	for {
 		select {
 		// Case when receiever caught the message
 		case msg := <-ch:
 			fmt.Printf("Consumer %v Received: %s\n", index, msg)
-		// Timeout in case the producer doesn't produce any more responses (In this example, 1000ms)
-		case <-time.After(1000 * time.Millisecond):
-			done = true
+			// Logical condition when receiver doesn't want to receive any more data
+			if index == 9 {
+				select {
+				// Send a signal to the moderator for ending the data tranmission
+				case toStopCn <- true:
+					fmt.Println("Stopped by receiver #", index)
+				default:
+				}
+				break consumerLoop
+			}
+		case <-stopCh:
+			break consumerLoop
 		}
 	}
 	wg.Done()
 }
 
 func main() {
-	multiConsumerProducer(20, 2)
+	multiConsumerProducer(20, 10)
 }
